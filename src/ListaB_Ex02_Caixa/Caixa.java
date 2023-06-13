2eaf2a54f475c261c685a1f9b98b83435b980e2f
thaisbreder <thais.breder@zup.com.br>
2023-06-13 09:23:01 -0300

    :sparkles: feat: lista B exercicio 02 finalizado

--- src/ListaB_Ex02_Caixa/Caixa.java
+++ src/ListaB_Ex02_Caixa/Caixa.java
@@ -1,4 +1,24 @@
 package ListaB_Ex02_Caixa;
 
 public class Caixa {
+    private double saldo;
+
+    public Caixa() {
+        this.saldo = 1000.0;
+    }
+
+    // Método para crédito
+    public void credito(double valor) {
+        saldo += valor;
+    }
+
+    // Método para débito
+    public void debito(double valor) {
+        saldo -= valor;
+    }
+
+    // Método saldo atual do caixa
+    public double saldoAtual() {
+        return saldo;
+    }
 }
--- src/ListaB_Ex02_Caixa/Main.java
+++ src/ListaB_Ex02_Caixa/Main.java
@@ -1,4 +1,37 @@
 package ListaB_Ex02_Caixa;
 
+import java.util.Scanner;
+
+
 public class Main {
+    public static void main(String[] args) {
+        Caixa caixa = new Caixa();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("----------------------------- CAIXA AUTOMÁTICO -----------------------------");
+
+        System.out.println("Quantos operações você deseja realizar? ");
+        int numOperacoes = scanner.nextInt();
+
+        for (int i = 1; i <= numOperacoes; i++) {
+            System.out.println("\nOperação " + i);
+            System.out.print("Digite 'C' para crédito ou 'D' para débito: ");
+            String tipo = scanner.next();
+
+            if (tipo.equalsIgnoreCase("C")) {
+                System.out.print("Digite o valor do crédito: ");
+                double valor = scanner.nextDouble();
+                caixa.credito(valor);
+            } else if (tipo.equalsIgnoreCase("D")) {
+                System.out.print("Digite o valor do débito: ");
+                double valor = scanner.nextDouble();
+                caixa.debito(valor);
+            } else {
+                System.out.println("Opção inválida!");
+                break;
+            }
+        }
+        System.out.println("\nSaldo final: R$" + caixa.saldoAtual());
+    }
+
 }
